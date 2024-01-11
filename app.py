f395881ece4bcfb4a6cbcb37d3366f980bf53b2b
Hein <huseyin.kla1@gmail.com>
2024-01-11 14:59:15 +0300

    flask api added

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import json
+from flask_cors import CORS
+from chat import systemMessage
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    return jsonify(liste = employees)
+
+@app.route('/systemAnswer', methods=['POST'])
+def get_info():
+    return jsonify({"systemMessage" : systemMessage(request.json['messageContent']) })
+
+if __name__ == '__main__':
+    app.run( host = '192.168.1.15', port = 5000 ,debug=True)
--- chat.py
+++ chat.py
@@ -25,8 +25,29 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
+def systemMessage(msg):
+    msg = tokenize(msg)
+    X = bag_of_words(msg, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    else:
+        return "I do not understand..."
+
+
 #Best Augmented Friend
-bot_name = "BAF"
+"""bot_name = "BAF"
 print("How are you? (type 'quit' to exit)")
 while True:
     sentence = input("You: ")
@@ -50,4 +71,4 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        print(f"{bot_name}: I do not understand...")"""
